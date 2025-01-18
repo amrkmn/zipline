0caa188aa3604a0f423e3d1685f7c7dc8f258383
diced <pranaco2@gmail.com>
2025-01-17 17:42:12 -0800

    fix: built version now reloads config changes

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,7 @@
 import glob from 'fast-glob';
 import { defineConfig } from 'tsup';
+import { replaceTscAliasPaths } from 'tsc-alias';
+import { copyFile, mkdir } from 'fs/promises';
 
 export default defineConfig(async (_) => {
   return [
@@ -12,6 +14,22 @@ export default defineConfig(async (_) => {
         ignore: ['./src/components/**/*.ts', './src/pages/**/*.ts'],
       }),
       outDir: 'build',
+      bundle: false,
+      onSuccess: async () => {
+        console.log('[ts] replacing ts paths...');
+        await replaceTscAliasPaths({
+          configFile: 'tsconfig.json',
+          outDir: 'build',
+        });
+
+        console.log('[built-ins] copying builtins...');
+        const builtins = await glob('./src/lib/theme/builtins/*.theme.json');
+
+        await mkdir('./build/lib/theme/builtins', { recursive: true });
+        for (const builtin of builtins) {
+          await copyFile(builtin, builtin.replace('./src/', './build/'));
+        }
+      },
     },
   ];
 });
